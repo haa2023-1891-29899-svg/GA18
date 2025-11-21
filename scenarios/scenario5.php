b4e0aa4da582cc1279bbce3145e6020c7067589e
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:41:11 +0800

    Validate email input before querying database
    Added a check for email presence in POST data to prevent errors.

--- scenarios/scenario5.php
+++ scenarios/scenario5.php
@@ -4,6 +4,9 @@ $conn = mysqli_connect("localhost","root","","class_db");
 // WRONG: misspelled POST key
 // $email = $_POST['emial'];
 
+if (!isset($_POST['email']) || $_POST['email'] === "") {
+    die("Error: No email provided in POST.");
+}
 $email = $_POST['email'];
 /*
 The input name is "email". The old code used "emial" which causes undefined index errors.
