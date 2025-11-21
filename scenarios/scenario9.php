ba91f9434118a5372a7ca636b1c861eb23fc438b
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:43:47 +0800

    Change from POST to GET for id retrieval
    Updated scenario9.php to validate GET request for 'id'.

--- scenarios/scenario9.php
+++ scenarios/scenario9.php
@@ -2,6 +2,9 @@
 // WRONG: expects POST but link sends GET
 // $id = $_POST['id'];
 
+if (!isset($_GET['id']) || $_GET['id'] === "" || !is_numeric($_GET['id'])) {
+    die("Error: No valid id supplied in the URL.");
+}
 $id = $_GET['id'];
 /*
 I changed POST to GET because the link (anchor tag) sends the value via URL, not via form.
