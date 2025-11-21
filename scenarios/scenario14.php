c0cda5eefecea7386a44ece477f4e997f8cd7e87
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:37:34 +0800

    Fix SQL insertion by adding input validation
    Added validation for required fields before SQL insertion.

--- scenarios/scenario14.php
+++ scenarios/scenario14.php
@@ -4,6 +4,14 @@ $data = $_POST;
 // WRONG: no quotes + incorrect array usage
 // VALUES ($data[first_name], $data[last_name], $data[email])
 
+if (
+    !isset($data['first_name']) || $data['first_name'] === "" ||
+    !isset($data['last_name']) || $data['last_name'] === "" ||
+    !isset($data['email']) || $data['email'] === ""
+) {
+    die("Missing one or more required fields: first_name, last_name, email");
+}
+
 $sql = "INSERT INTO students (first_name, last_name, email)
         VALUES ('{$data['first_name']}', '{$data['last_name']}', '{$data['email']}')";
 /*
