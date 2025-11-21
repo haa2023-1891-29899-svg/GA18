5710e38104b2af85237d65787304dde5ae0b0f93
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:38:08 +0800

    Add validation for maximum page number
    Added validation to limit maximum page number to 1000.

--- scenarios/scenario15.php
+++ scenarios/scenario15.php
@@ -1,11 +1,14 @@
 <?php
-$page = $_GET['page'];
+$page = isset($_GET['page']) ? $_GET['page'] : 0;
 $limit = 5;
 
 // WRONG: no validation
 // user can enter huge page number
 
 $page = max(0, intval($page));
+if ($page > 1000) {
+    die("Page number too large for performance reasons.");
+}
 $offset = $page * $limit;
 
 $sql = "SELECT * FROM students LIMIT $offset, $limit";
