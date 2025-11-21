0d39a023b64a5006b26a1c368b481004bcbadb2a
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:37:06 +0800

    Refactor scenario13.php to include email validation
    Added email validation and connection setup.

--- scenarios/scenario13.php
+++ scenarios/scenario13.php
@@ -1,9 +1,13 @@
 <?php
-$newEmail = $_POST['email'];
+$conn = mysqli_connect("localhost","root","","class_db");
+$newEmail = isset($_POST['email']) ? $_POST['email'] : null;
 
 // WRONG: NO WHERE CLAUSE!
 // $sql = "UPDATE students SET email='$newEmail'";
 
+if (empty($newEmail)) {
+    die("Email must be provided.");
+}
 $sql = "UPDATE students SET email='$newEmail' WHERE student_id=1";
 /*
 I added a WHERE clause to avoid updating ALL rows.
