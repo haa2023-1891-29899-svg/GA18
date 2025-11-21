38731e455ce9577ff0fcbfff8292e7905c47349e
Hezekiah Agsunod <haa2023-1891-29899@bicol-u.edu.ph>
2025-11-21 14:43:03 +0800

    Implement SQL error handling in scenario8.php
    Added error handling for SQL query execution.

--- scenarios/scenario8.php
+++ scenarios/scenario8.php
@@ -7,6 +7,10 @@ $res = mysqli_query($conn,"SELECT * FROM students");
 // $row = mysqli_fetch_assoc($res);
 // echo $row['email'];
 
+if (!$res) {
+    die("SQL Error: " . mysqli_error($conn));
+}
+
 while ($row = mysqli_fetch_assoc($res)) {
     echo $row['email'] . "<br>";
 }
@@ -16,3 +20,4 @@ I added a while loop because mysqli_fetch_assoc returns one row at a time.
 A loop is required to print all records.
 */
 ?>
+
